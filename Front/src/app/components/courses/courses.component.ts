9cbde1380f3980ad3cd426b9e55de6808619362f
youssef sahnoun <youssef.sahnoun@insat.ucar.tn>
2025-02-05 00:33:13 +0100

    added enrollement verification front and back , improved coursedetail styles, and added category filtering

--- Front/src/app/components/courses/courses.component.ts
+++ Front/src/app/components/courses/courses.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from '../../services/course.service';
 import { Course } from '../../models/course.model';
-
+import { ApiService } from '../../services/api.service';
+import { Category } from '../../models/category.model';
 @Component({
   selector: 'app-home',
   templateUrl: './courses.component.html',
@@ -11,8 +12,11 @@ export class CoursesComponent implements OnInit {
   courses: Course[] = [];
   filteredCourses: Course[] = [];
   searchQuery: string = '';
-
-  constructor(private courseService: CourseService) {}
+  categories: Category[] = [];
+  selectedCategory: string = '';
+  constructor(private courseService: CourseService,
+              private CategoryService: ApiService
+  ) {}
 
   ngOnInit(): void {
     this.courseService.getCourses().subscribe({
@@ -25,12 +29,31 @@ export class CoursesComponent implements OnInit {
         console.error('Error loading courses in component:', error);
       }
     });
+    this.CategoryService.getAllCategories().subscribe({
+      next: (data) => {
+        this.categories = data;
+        console.log('Categories loaded in component:', this.categories);
+      },
+      error: (error) => {
+        console.error('Error loading categories in component:', error);
+      }
+    });
   }
   filterCourses(): void {
-    this.filteredCourses = this.courses.filter(course =>
-      course.title.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-      course.content.toLowerCase().includes(this.searchQuery.toLowerCase())
-    );
+    console.log('Selected Category:', this.selectedCategory); // Debugging
+    console.log('All Courses:', this.courses); // Debugging
+  
+    this.filteredCourses = this.courses.filter(course => {
+      const matchesSearch = course.title.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+                            course.content.toLowerCase().includes(this.searchQuery.toLowerCase());
+      const matchesCategory = this.selectedCategory === '' || course.category?.title === this.selectedCategory;
+  
+      console.log('Course:', course.title, 'Matches Search:', matchesSearch, 'Matches Category:', matchesCategory); // Debugging
+  
+      return matchesSearch && matchesCategory;
+    });
+  
+    console.log('Filtered Courses:', this.filteredCourses); // Debugging
   }
  
 }
