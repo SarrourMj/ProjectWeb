fbb6ed8b362d44530cbaf1b2a4602c52795417f8
bdnn <youssef.bouden2002@gmail.com>
2025-02-05 06:51:44 +0100

    Redirect successfully signed up users to courses page

--- Front/src/app/components/signup/signup.component.ts
+++ Front/src/app/components/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SignupService } from '../../services/signup.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -10,7 +11,7 @@ import { SignupService } from '../../services/signup.service';
 export class SignupComponent {
   signupForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private signupService: SignupService) {
+  constructor(private fb: FormBuilder, private signupService: SignupService, private router: Router) {
     this.signupForm = this.fb.group({
       username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -21,7 +22,9 @@ export class SignupComponent {
   onSubmit() {
     if (this.signupForm.valid) {
       this.signupService.signup(this.signupForm.value).subscribe({
-        next: (response) => console.log('Signup successful', response),
+        next: (response) => {console.log('Signup successful', response);
+          this.router.navigate(['/courses']);
+        },
         error: (err) => console.error('Signup error', err),
       });
     }
