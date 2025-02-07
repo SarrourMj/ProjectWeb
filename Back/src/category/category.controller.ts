4bd348a7e83e5e57b749eb7d2658a2b3602e8a92
Zied-BenBahri <ziedbenbahri@gmail.com>
2025-02-08 00:56:10 +0100

    adding guard

--- Back/src/category/category.controller.ts
+++ Back/src/category/category.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Roles } from '../auth/assets/roles.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
+  @UseGuards(RolesGuard)
+  @Roles('admin')
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoryService.create(createCategoryDto);
@@ -22,11 +28,15 @@ export class CategoryController {
     return this.categoryService.findOne(+id);
   }
 
+  @UseGuards(RolesGuard)
+  @Roles('admin')
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
     return this.categoryService.update(+id, updateCategoryDto);
   }
 
+  @UseGuards(RolesGuard)
+  @Roles('admin')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.categoryService.remove(+id);
--- Back/src/user/user.controller.ts
+++ Back/src/user/user.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { Course } from 'src/course/entities/course.entity';
 import { Chapter } from 'src/chapter/entities/chapter.entity';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Roles } from '../auth/assets/roles.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+  
+  @UseGuards(JwtAuthGuard)
   @Post(':userId/increment-score/:score')
   async incrementUserScore(
     @Param('userId') userId: number,
@@ -20,30 +25,40 @@ export class UserController {
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
   }
+  
+  @UseGuards(JwtAuthGuard)
   @Get(':id/score')
   async getScore(@Param('id') id: number) {
   const user = await this.userService.findOne(id);
   return user.score; // Return only the score
   }
+
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.userService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
   }
 
+  @UseGuards(RolesGuard)
+  @Roles('admin')
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.userService.remove(id);
   }
+  
+  @UseGuards(JwtAuthGuard)
   @Post(':userId/enroll/:courseId')
   async enrollCourse(
     @Param('userId') userId: number,
@@ -51,6 +66,8 @@ export class UserController {
   ): Promise<User> {
     return this.userService.enrollCourse(userId, courseId);
   }
+
+  @UseGuards(JwtAuthGuard)
   @Post(':userId/complete/:chapterId')
   async completeChapter(
     @Param('userId') userId: number,
@@ -59,10 +76,13 @@ export class UserController {
     return this.userService.completeChapter(userId, chapterId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':userId/courses')
   async getUserCourses(@Param('userId') userId: number): Promise<Course[]> {
     return this.userService.getUserCourses(userId);
   }
+
+  @UseGuards(JwtAuthGuard)
   @Get(':userId/chapters')
   async getUserChapters(@Param('userId') userId: number): Promise<Chapter[]> {
     return this.userService.getUserChapters(userId);
